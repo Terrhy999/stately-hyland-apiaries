c26618ef195c67d341a453ad79be0a8db6fc9f8c
Terrhy999 <thyla999@gmail.com>
2022-03-02 21:06:19 -0500

    Added structure for Header component

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="flex flex-col bg-[url('https://statelyhylandmanor.com/wp-content/uploads/2020/08/cropped-20200605_200148-PANO-scaled-1.jpg')] bg-center bg-no-repeat bg-cover w-full">
+      <div className="flex flex-row justify-center items-center p-1 px-4 z-20 h-96">
+        <div className="bg-[#1d1d1d] divide-y-2 divide-[rgba(255, 255, 255, 0.1)] p-4">
+          <h6 className="font-bold font-Raleway tracking-wider text-4xl text-center m-4 text-white">
+            STATELY HYLAND APIARIES
+          </h6>
+          <h2 className="text-white font-Raleway text-xl text-center p-4">
+            Seasoned, Orginizatioal Wisdom ~ from a mom of three boys.
+          </h2>
+        </div>
+      </div>
+      <div className="bg-[#1d1d1d] py-3 flex justify-center">
+        <div className="flex flex-row w-full font-lato max-w-xl">
+          <Link href="/">
+            <a
+              className={`p-2 mx-1 hover:text-white rounded text-lg ${
+                router.pathname === "/"
+                  ? "text-white"
+                  : "text-[rgba(255, 255, 255, 0.5)]"
+              }`}
+            >
+              Home
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
