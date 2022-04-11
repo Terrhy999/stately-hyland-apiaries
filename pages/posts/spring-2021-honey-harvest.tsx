515c629c8d0c3f77e3ee8b38d6a89b830bd5b789
Terrhy999 <thyla999@gmail.com>
2022-04-11 13:37:03 -0400

    Added blog post

--- pages/posts/spring-2021-honey-harvest.tsx
+++ pages/posts/spring-2021-honey-harvest.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import image1 from "../../public/images/spring-2021-honey-harvest/1-backyard.webp";
 import image2 from "../../public/images/spring-2021-honey-harvest/2-hive.webp";
 import image3 from "../../public/images/spring-2021-honey-harvest/3-hive.webp";
+import image4 from "../../public/images/spring-2021-honey-harvest/4-hive.webp";
+import image5 from "../../public/images/spring-2021-honey-harvest/5-bee-escape.webp";
+import image6 from "../../public/images/spring-2021-honey-harvest/6-honey-bucket.webp";
 
 const postPage = () => {
   return (
     <div className="flex w-full flex-grow justify-center pt-3">
-      <div className="prose font-lato">
+      <div className="prose prose-lg font-lato">
         <h1>Spring 2021 Honey Harvest</h1>
         <div>
           <span>June 13, 2021 / </span>
@@ -37,20 +40,20 @@ const postPage = () => {
           </blockquote>
           <p>
             I took the opportunity of setting up a time lapse camera to document
-            our labors of{" "}
+            our labors of
             <span className="underline">
               “Pulling Honey” at Stately Hyland Apiaries
             </span>
-            , Spring 2021. (View the full video{" "}
+            , Spring 2021. (View the full video
             <a href="https://youtu.be/3Stqts62oe0">here</a>) Assisting me, is my
-            youngest son, who is 19 and also studying Beekeeping{" "}
+            youngest son, who is 19 and also studying Beekeeping
             <span className="italic">
               (currently pursuing Penn State Beginning Beekeeping Online
               Course!)
-            </span>{" "}
+            </span>
             Not only is it exciting to share all my knowledge with my son, I
             have the added benefit of another set of eyes, hands and MUSCLES to
-            help me with my more labor intensive tasks, like carrying;{" "}
+            help me with my more labor intensive tasks, like carrying;
             <span className="italic">
               ~ honey laden supers bursting with glistening light golden
               deliciousness ~
@@ -61,7 +64,7 @@ const postPage = () => {
             The video begins on a steamy, humid 92° morning. We are wearing our
             protective bee suits. The first and foremost important part of
             Beekeeping, is to have a plan and all your tools in place. That is
-            what we are doing- setting up our{" "}
+            what we are doing- setting up our
             <a href="https://hivebutler.com/products/hive-butler-hive-tote-and-lid">
               Hive Butlers
             </a>
@@ -91,6 +94,115 @@ const postPage = () => {
           <div className="flex justify-center">
             <Image src={image3} alt="Beehive #2" />
           </div>
+          <p>
+            The third hive that we took from was called the “Borg Hive”. Aptly
+            named in 2019 by our former State Apiarist Tim Schuler. My two
+            beginning beekeeping hives were not faring well from the nucs that
+            they had started from. As a result, and at the advice of the State
+            Apiarist who was inspecting them, he put the two hives together and
+            educated me saying
+            <span
+              className="italic
+            "
+            >
+              “they are like the &apos;Borg&apos; - Assimilate or die.”
+            </span>
+            Hence, the Borg Hive, another successful hive since 2019!
+          </p>
+          <div className="flex justify-center">
+            <Image src={image4} alt="Beehive #3" />
+          </div>
+          <p>
+            Together with my son, we pulled two 9 frame mediums, one 5 frame
+            medium, 4 individual deep frames for a total of
+            <span className="font-bold"> 132 pounds of honey </span>
+            in our 2021 Spring Season!
+          </p>
+          <p>
+            I created a fast motion video with captioning explaining our
+            movements. This blog article also fills in some of the blanks and
+            adds explanations- as the video is almost 12 minutes long, this
+            article will serve to &apos;sum up&apos; our adventure.
+          </p>
+          <p>
+            EJH and I both wear our bee suits for safety. We have to remove very
+            heavy boxes of honey frames{" "}
+            <span className="italic">
+              (that can weigh almost 5 pounds each)
+            </span>
+            for extraction. To do this, they are taken up the hill from the
+            apiary to my car. I extract my honey with another beekeeper. I put
+            individual frames of honey into our “Hive Butlers” that hold the
+            dripping honey and keep the bees out. The other boxes need to have
+            straggler bees &apos;brushed&apos; off and transferred to a
+            &apos;bee free&apos; box.
+          </p>
+          <p>
+            We remove most of the bees from these boxes by way of a “Bee
+            Escape”.
+          </p>
+          <div className="flex justify-center">
+            <Image src={image5} alt="Bee Escape Board" />
+          </div>
+          <p>
+            It is a special board placed between the Honey Supers on top of the
+            hive and the “Brood Boxes” below. The bees naturally migrate to the
+            lower part of the hive during the cool nights to help warm the brood
+            nest. As they navigate the “Bee Escape” they can exit down, but not
+            go back up to the honey boxes again.{" "}
+            <span className="italic">(Think of it as a bee turnstile!)</span> As
+            such, the honey boxes are nearly bee free and can be easily taken
+            from the hives. We take out each individual frame and gently brush
+            the bees to the front of the hive{" "}
+            <span className="italic">
+              (where they fly back in and get back to work again!)
+            </span>{" "}
+            The Honey Supers have 9 frames of honey that painstakingly have to
+            be removed and brushed off.
+          </p>
+          <p>
+            You will see us using the “Smoker” to keep the bees calm{" "}
+            <span className="italic">(busy drinking honey)</span> while we
+            remove their surplus honey.{" "}
+            <span className="italic">
+              Oh, and by the way, the JAZZY music is to keep you attending
+              during the long 12 minute long video!
+            </span>{" "}
+            As you can see in the video, some of the honey is “uncapped”.{" "}
+            <span className="italic">
+              (No wax cappings on the top of the honeycomb.)
+            </span>{" "}
+            We test the honey for the precise humidity using a “Refractometer”
+            and can assure that the honey was a precise 16% humidity, perfect
+            for extracting!
+          </p>
+          <Image src={image6} alt="Bucket of extracted honey" />
+          <p>
+            There is no doubt about it- there are bees flying everywhere during
+            this process (and some even crawling ~ a daunting thought with loose
+            pants cuffs, but I digress…)
+          </p>
+          <p>
+            We remove the honey supers every time they are filled (which could
+            be three times a season!), extract them and return them to the
+            hives. The alternative would be to stack them higher and higher on
+            the hive and that would be unwieldy and difficult to manage. This is
+            our beautifully light colored “Spring 2021 Honey” (my personal
+            favorite!) We hope to get another Summer nectar flow and possibly
+            even an Autumn flow again in September!
+          </p>
+          <p>
+            Stately Hyland Apiaries will have an abundance of honey and products
+            this year that can be perused on our website
+            “StatelyHylandApiaries.Com“. They can also be found on our front
+            fence, every (sunny) day!{" "}
+          </p>
+          <p>
+            Beekeeping is not glamorous by any means. It is hot, tedious,
+            tenacious work! It truly is a “Labor of Love”… for the love of
+            honey, and bees ~ from our wonderful Marquette Hives at Stately
+            Hyland Apiary! Thank you for your visit today!
+          </p>
         </div>
       </div>
     </div>
