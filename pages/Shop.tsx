93044675863e02a6721032f83ae32b3091c96ed7
Terrhy999 <thyla999@gmail.com>
2022-06-17 16:07:25 -0400

    Removed 10 item limit on api requests and commented out filtering code for shop page

--- pages/Shop.tsx
+++ pages/Shop.tsx
@@ -13,23 +13,28 @@ export async function getStaticProps() {
     apiVersion: "2020-08-27",
   });
 
-  const productsObject = await stripe.products.list({ active: true });
+  const productsObject = await stripe.products.list({
+    active: true,
+    limit: 100,
+  });
   const products = productsObject.data;
 
-  const pricesObject = await stripe.prices.list();
+  const pricesObject = await stripe.prices.list({ limit: 100 });
   const prices = pricesObject.data;
 
   const productsWithPrices: IProduct[] = products.map((product) => {
-    const priceForProductObject = prices.find(
+    const priceObjectForProduct = prices.find(
       (price) => price.product === product.id
     );
-    if (priceForProductObject == null) {
-      throw new Error("No product with this price");
+    if (priceObjectForProduct == null) {
+      throw new Error(`Product ${product.name} has no associated price object`);
     }
 
-    const priceForProduct = priceForProductObject["unit_amount"];
+    const priceForProduct = priceObjectForProduct["unit_amount"];
     if (priceForProduct == null) {
-      throw new Error("Unit Amount property is null");
+      throw new Error(
+        `Price ${priceObjectForProduct.id} is missing "unit_amount" property`
+      );
     }
 
     if (product.metadata["productType"] == null) {
@@ -49,7 +54,7 @@ export async function getStaticProps() {
     const finalProduct: IProduct = {
       name: product.name,
       productId: product.id,
-      priceId: priceForProductObject["id"],
+      priceId: priceObjectForProduct["id"],
       unitAmount: priceForProduct,
       images: product.images,
       description: product.description ?? "",
@@ -68,56 +73,56 @@ export async function getStaticProps() {
 }
 
 const Shop = ({ products }: { products: IProduct[] }) => {
-  interface IFilter {
-    honey: boolean;
-    candle: boolean;
-    lightHoney: boolean;
-    darkHoney: boolean;
-    creamedHoney: boolean;
-  }
+  // interface IFilter {
+  //   honey: boolean;
+  //   candle: boolean;
+  //   lightHoney: boolean;
+  //   darkHoney: boolean;
+  //   creamedHoney: boolean;
+  // }
 
-  const initialFilter: IFilter = {
-    honey: true,
-    candle: true,
-    lightHoney: true,
-    darkHoney: true,
-    creamedHoney: true,
-  };
+  // const initialFilter: IFilter = {
+  //   honey: true,
+  //   candle: true,
+  //   lightHoney: true,
+  //   darkHoney: true,
+  //   creamedHoney: true,
+  // };
 
-  type category =
-    | "honey"
-    | "candle"
-    | "lightHoney"
-    | "darkHoney"
-    | "creamedHoney";
+  // type category =
+  //   | "honey"
+  //   | "candle"
+  //   | "lightHoney"
+  //   | "darkHoney"
+  //   | "creamedHoney";
 
-  const [productFilters, setProductFilters] = useState(initialFilter);
-  const [displayedProducts, setDisplayedProducts] = useState(products);
+  // const [productFilters, setProductFilters] = useState(initialFilter);
+  // const [displayedProducts, setDisplayedProducts] = useState(products);
 
-  const updateFilters = (category: category) => {
-    const newFilters: IFilter = {
-      ...productFilters,
-      [category]: !productFilters[category],
-    };
+  // const updateFilters = (category: category) => {
+  //   const newFilters: IFilter = {
+  //     ...productFilters,
+  //     [category]: !productFilters[category],
+  //   };
 
-    setProductFilters(newFilters);
+  //   setProductFilters(newFilters);
 
-    setDisplayedProducts(getFilteredProducts(newFilters));
-  };
+  //   setDisplayedProducts(getFilteredProducts(newFilters));
+  // };
 
-  const getFilteredProducts = (filters: IFilter) => {
-    const enabledFilters = Object.keys(filters).filter(
-      (key) => filters[key as keyof IFilter]
-    );
+  // const getFilteredProducts = (filters: IFilter) => {
+  //   const enabledFilters = Object.keys(filters).filter(
+  //     (key) => filters[key as keyof IFilter]
+  //   );
 
-    const filteredProducts = products.filter((product) => {
-      if (enabledFilters.includes(product.metadata["productType"])) {
-        return product;
-      }
-      return;
-    });
-    return filteredProducts;
-  };
+  //   const filteredProducts = products.filter((product) => {
+  //     if (enabledFilters.includes(product.metadata["productType"])) {
+  //       return product;
+  //     }
+  //     return;
+  //   });
+  //   return filteredProducts;
+  // };
 
   return (
     <>
@@ -126,10 +131,10 @@ const Shop = ({ products }: { products: IProduct[] }) => {
       </Head>
       <div className="w-full">
         <h2 className="font-lato text-3xl font-bold pb-5">Shop</h2>
-        <div className="bg-white shadow-lg p-2 mb-5 rounded-md flex flex-row justify-between items-center hover:bg-gray-50">
+        {/* <div className="bg-white shadow-lg p-2 mb-5 rounded-md flex flex-row justify-between items-center hover:bg-gray-50">
           <span>Filter</span>
           <FaAngleDown />
-        </div>
+        </div> */}
         {/* <div className="shadow-lg bg-white">
           <p className="hover:bg-gray-300">All</p>
           <p className="hover:bg-gray-300">Honey</p>
@@ -137,7 +142,7 @@ const Shop = ({ products }: { products: IProduct[] }) => {
         </div> */}
         <div className="min-w-full flex flex-col justify-center">
           <div className="grid grid-cols sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-            {displayedProducts.map((product) => (
+            {products.map((product) => (
               <ProductCard product={product} key={product.productId} />
             ))}
           </div>
