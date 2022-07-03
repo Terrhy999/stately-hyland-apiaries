e8abb68c3c7330ea71bfdfd38f1751b28b6eb78e
Terrhy999 <thyla999@gmail.com>
2022-07-03 18:42:00 -0400

    removed some code from _document.tsx that honestly idk what it did

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,10 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static override async getInitialProps(ctx: DocumentContext) {
-    const originalRendererPage = ctx.renderPage;
-
-    ctx.renderPage = () =>
-      originalRendererPage({
-        enhanceApp: (App) => App,
-        enhanceComponent: (Component) => Component,
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
-  }
-
   override render() {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
@@ -34,7 +13,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
-          <meta charSet="UTF-8" />
         </Head>
         <body className="h-full">
           <Main />
